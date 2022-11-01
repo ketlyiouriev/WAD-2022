af5753bf3f10dd83bff4b0b67e694012b8b7fde7
ketlyiouriev <ketly.iouriev@ut.ee>
2022-11-01 18:01:01 +0200

    updated javascript file

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,33 @@
-
 import posts from '/res/json/posts.json' assert {type: 'json'};
-
 
 window.onload = function() {
     fetch('http://myjson.dit.upm.es/api/bins/e7re')
         .then((response) => response.json())
-        .then(posts => {
-            console.log(posts);
-            appendData(posts);
+        .then(data => {
+            console.log(data);
+            appendData(data);
 
-            function appendData(posts) {
-                console.log("jõudsin siia!")
+            function appendData(data) {
                 var mainContainer = document.getElementsByClassName("main-body");
                 for (var i = 0; i < data.length; i++) {
                     var post_box = document.createElement("div");
-                    create_header_content(i, mainContainer);
-                    create_body_content(i, mainContainer);
-                    create_footer_content(i, mainContainer);
+                    create_header_content(i, post_box);
+                    create_body_content(i, post_box);
+                    create_footer_content(i, post_box);
                     mainContainer.appendChild(post_box);
                 }
-              }
+            }
     
-            function create_header_content(i, mainContainer) {
+            function create_header_content(i, post_box) {
                 var div_header = document.createElement("div");
                 var img = document.createElement("img");
-                
-                img.appendChild(posts[i].user_icon);
+                img.src = posts[i].user_icon;
                 div_header.appendChild(img);
                 var p = document.createElement("p");
-                p.appendChild(posts[i].datetime);
+                var text_node = document.createTextNode(posts[i].datetime);
+                p.appendChild(text_node);
                 div_header.appendChild(p);
-                mainContainer.appendChild(div_header);
+                post_box.appendChild(div_header);
             }
     
             function create_body_content(i, mainContainer) {
