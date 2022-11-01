4f42079099d8cdb5377f40830895825931c18324
vandapakina <vanda.pakina@ut.ee>
2022-11-01 15:55:17 +0200

    script.js modified

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,32 @@
+import posts from 'res/json/posts.json' assert {type: 'json'};
+
+
+console.log(posts.user_name)
+
+window.onload = function() {
+    fetch(posts)
+        .then((response) => response.json())
+        .then(json => {
+            console.log(json);
+
+        })
+}
+
+/*
 window.onload = function() {
 
 
 
+    
     fetch('http://myjson.dit.upm.es/api/bins/e7re')
         .then((response) => response.json())
         .then(json => {
             console.log(json);
 
         })
+    
 
 
 
 }
+*/
