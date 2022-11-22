b88abb9c43494381cbde6d9c2db819350c6b2388
ketlyiouriev <ketly.iouriev@ut.ee>
2022-11-22 21:27:08 +0200

    minor changes

--- hw3-vue/src/App.vue
+++ hw3-vue/src/App.vue
@@ -12,3 +12,13 @@ export default {
 components: {Header, Footer}
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
