6bef62c6b592e90eff43761570444b171a046253
ketlyiouriev <ketly.iouriev@ut.ee>
2022-12-19 00:08:00 +0200

    minor changes related to using posts

--- backend-hw4/server.js
+++ backend-hw4/server.js
@@ -202,6 +202,46 @@ app.delete('/posts', async(req, res) => {
     }
 });
 
+app.get('/posts/:id', async(req, res) => {
+    try {
+        console.log("get a post with route parameter request has arrived");
+        const { id } = req.params;
+        const posts = await pool.query(
+            "SELECT * FROM posts WHERE id = $1", [id]
+        );
+        res.json(posts.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.put('/posts/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const post = req.body;
+        console.log("update request has arrived");
+        const updatepost = await pool.query(
+            "UPDATE posts SET body = $2 WHERE id = $1 RETURNING*", [id, post.body]
+        );
+        res.json(updatepost);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.delete('/posts/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        console.log("delete a post request has arrived");
+        const deletepost = await pool.query(
+            "DELETE FROM posts WHERE id = $1 RETURNING*", [id]
+        );
+        res.json(deletepost);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
 
 ///////////////////////////////////////////////////////////
 
